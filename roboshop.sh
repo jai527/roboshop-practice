b032487646ab8e8c40af8e8bdd12e74283ef2c42
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-11 17:23:15 +0530

    hi

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,8 @@ do
    --query 'Instances[0].InstanceId' \
    --output text )
 
-    if [ $instance == "frontend" ]; then
+    if [ $instance == "frontend" ];then
+    
         IP=$(
         aws ec2 describe-instances \  
         instance-ids $Instance_ID \
