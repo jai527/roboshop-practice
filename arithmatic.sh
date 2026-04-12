f2351a638d53398328faac9ec473c5c3f27d44c5
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-12 21:41:03 +0530

    hello

--- arithmatic.sh
+++ arithmatic.sh
@@ -2,7 +2,7 @@ a=10
 b=26
 c=30
 
-#{echo "additional valve is a : a + b = $((a + b))"
+echo "additional valve is a : a + b = $((a + b))"
 echo "a - b = $((a - b))"
 echo "a * b = $((a * b))"
 echo "a / b = $((a / b))"
@@ -16,11 +16,9 @@ fi
  if [ $b -gt $a ]; then 
  echo "b is greater then b"
  fi
-  if [ $c -lt $b ]; then
+if [ $c -lt $b ]; then
     echo "c is less then b"
-fi}#
-
-
+fi
 if [ $a -eq $b ] && [ $b -eq $a ]; then
 
     echo "aleast one condition is true"
