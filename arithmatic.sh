6deccece993e22d673980876d8f62a63b65b7e77
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-12 07:28:27 +0530

    hello

---
+++ arithmatic.sh
@@ -0,0 +1,10 @@
+a=10
+b=26
+c=30
+
+echo "a + b = $((a + b))"
+echo "a - b = $((a - b))"
+echo "a * b = $((a * b))"
+echo "a / b = $((a / b))"
+echo "a % b = $((a % b))"
+echo "a ** 2 = $((a ** 2))"
