c2d6654e1cff877cfc425a772ce1654d3da7bccb
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-12 17:58:54 +0530

    hello

--- arithmatic.sh
+++ arithmatic.sh
@@ -10,12 +10,12 @@ echo "a % b = $((a % b))"
 echo "a ** 2 = $((a ** 2))"
 read -p "Enter a number: " number
 echo "number + c = $((number + c))"
- if [$a -eq $b]; then
+ if [ $a -eq $b ]; then
    echo "a is eqal to b"
 fi
- if [$b -gt $a]; then 
+ if [ $b -gt $a ]; then 
  echo "b is greater then b"
  fi
-  if [$c -lt $b]; then
+  if [ $c -lt $b ]; then
     echo "c is less then b"
 fi
