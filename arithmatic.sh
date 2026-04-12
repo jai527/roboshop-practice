9bc2bd3b85dfe57de1fc9731937a5557a905041e
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-12 21:46:45 +0530

    hello

--- arithmatic.sh
+++ arithmatic.sh
@@ -5,5 +5,13 @@ echo "arithatic operations"
 
 if [ $a -eq 10 ] && [ $b -eq 26 ]; then
 
-    echo "aleast one condition is true"
+    echo "both condition is true"
 fi
+
+if [ $c -lt 40 ] && [ $b -lt 20 ]; then
+   
+   echo "atlest one condtions is ture"
+fi
+ else
+  
+   echo "one condition is false"
