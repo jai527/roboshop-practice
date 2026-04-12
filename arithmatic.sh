417315d80abbbdd0e46cbaaa60154ab37936902f
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-12 21:41:55 +0530

    hello

--- arithmatic.sh
+++ arithmatic.sh
@@ -2,23 +2,6 @@ a=10
 b=26
 c=30
 
-echo "additional valve is a : a + b = $((a + b))"
-echo "a - b = $((a - b))"
-echo "a * b = $((a * b))"
-echo "a / b = $((a / b))"
-echo "a % b = $((a % b))"
-echo "a ** 2 = $((a ** 2))"
-read -p "Enter a number: " number
-echo "number + c = $((number + c))"
- if [ $a -eq $b ]; then
-   echo "a is equal to b"
-fi
- if [ $b -gt $a ]; then 
- echo "b is greater then b"
- fi
-if [ $c -lt $b ]; then
-    echo "c is less then b"
-fi
 if [ $a -eq $b ] && [ $b -eq $a ]; then
 
     echo "aleast one condition is true"
