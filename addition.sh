cadab99c1d6cc6926a3488b870e4abe2e8877fc7
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-13 22:51:21 +0530

    helo

--- addition.sh
+++ addition.sh
@@ -4,5 +4,8 @@ sum=$(($a + $b))
 echo "addition of two valves $sum"
 product=$(($a * $b))
 echo "multiplie two valves $product"
-increment=$(($a  $b))
-echo "increment% $increment"
+
+for((i=1; i<=5; i++))
+do
+ echo "interation $i
+done
