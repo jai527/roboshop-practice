06edd5f450e2faed888e4288d5a3a01e2085faae
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-13 16:55:37 +0530

    helo

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-    Instance_Id=$(aws ec2 run-instances \
+    Instance_Id=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
