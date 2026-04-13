92c12eca53aa1eeb88ce25625ffcf4dfe840463f
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-13 16:45:29 +0530

    helo

--- roboshop.sh
+++ roboshop.sh
@@ -2,15 +2,30 @@
 
 SG_ID="sg-02677d7f13c97966d"
 AMI_ID="ami-0220d79f3f480ecf5"
+InstanceId
 
 for instance in $@
 do
-    aws ec2 run-instances \
+    Instance_Id=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --count 1 \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyInstance}]"
-    --query 'Instances[0].Reservation[0].PrivateIpAddress' \
-    --output.txt 
-done    
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output.txt)
+
+    if [ $instance == "frontend" ]; then
+       IP=$(aws ec2 describe-instances \
+       --instance-ids $Instance_Id \
+       --query "Reservations[].Instances[].PublicIpAddress" \
+       --output text)
+    else
+       [ $instance == "frontend" ]; then
+       IP=$(aws ec2 describe-instances \
+       --instance-ids $Instance_Id \
+       --query "Reservations[].Instances[].PrivateIpAddress" \
+       --output text)
+    fi
+       
+done
