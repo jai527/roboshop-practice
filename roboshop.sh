a081ecb9c0e9e4f9aa8e9906b5b0d3fdfab92b7c
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-13 16:14:06 +0530

    helo

--- roboshop.sh
+++ roboshop.sh
@@ -2,33 +2,15 @@
 
 SG_ID="sg-02677d7f13c97966d"
 AMI_ID="ami-0220d79f3f480ecf5"
-INSTANCE_TYPE="t3.micro"
-#ZONE_ID="Z005336437MHJ16KCQQI8"
-#DOMAIN="jai01.online"
 
 for instance in $@
 do
-   INSTANCE_ID=$( aws ec2 run-instances \
-   --image-id $AMI_ID \
-   --instance-type $INSTANCE_TYPE \
-   --security-group-ids $SG_ID \
-   --tag-specifications \"ResourceType=instance,Tags=[{Key=Name,Value=$TAG-$instance}]" \
-   --query 'Instances[0].InstanceId' \
-   --output text )
-
-    if [ $instance == "frontend" ]; then
-            IP=$(
-            aws ec2 describe-instances \  
-            instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text
-        )
-    else
-            IP=$(
-            aws ec2 describe-instances \      
-            instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text
-        )
-    fi
-Done
+    aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type t3.micro \
+    #--key-name MyKeyPair \
+    --security-group-ids $SG_ID \
+    --query 'Instances[0].PrivateIpAddress' \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=MyInstance}]"
+done    
