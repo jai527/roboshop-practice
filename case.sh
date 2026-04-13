52cde48dd56c0460a38e064b3c4d71831e00b5a2
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-13 13:00:08 +0530

    helo

--- case.sh
+++ case.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 day="monday"
+day1="sunday"
 
 case $day in 
    monday | tuesday | wednesday| thursday | friday) 
     echo "the $day is weekday"
     ;;
    sunday  | saturday)
-    echo "$day is weekday"
+    echo "$day1 is sunday"
     ;;
     *)
 esac
