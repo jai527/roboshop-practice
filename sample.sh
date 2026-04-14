3bc07c2027c419d811c3fcf9abc72770bae432ba
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-14 16:53:15 +0530

    helo

--- sample.sh
+++ sample.sh
@@ -6,5 +6,5 @@ read r
 echo  "pleae enter the time valve"
 read t
 echo "calculating the valve "
-total_valve=$(p * r * t \100)
+total_valve=$((p * r * t \100))
 echo "simple intrest rate: $total_valve"
