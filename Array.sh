228c1382a0bd448c79a7a0d9f25a64eb38863d97
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-14 15:33:17 +0530

    helo

---
+++ Array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+array_name=("subramani" "shyamala" "kushika" "kunshika" "sangeetha" "jai" "rudhra")
+
+echo "${array_name[0]}"
+echo "${array_name[1]}"
+echo "${array_name[2]}"
+echo "${array_name[3]}"
+echo "${array_name[4]}"
+echo "${array_name[5]}"
+echo "${array_name[6]}"
+echo "${array_name[7]}"
+echo "all family members and joint family"
