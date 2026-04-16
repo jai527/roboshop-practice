75241efda3da4684ac921579ab86104a590f0530
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-16 06:53:53 +0530

    hello

--- practice.sh
+++ practice.sh
@@ -7,5 +7,24 @@ read street
 if [ "$village_name" == "myvillage" ]; then
     echo "correct village"
 else
-    echo "wrong village"
+    echo " village"
 fi
+
+
+echo "Enter village name:"
+read village_name
+
+case "$village_name" in
+    village1)
+        echo "Village 1 selected"
+        ;;
+    village2)
+        echo "Village 2 selected"
+        ;;
+    village3)
+        echo "Village 3 selected"
+        ;;
+    *)
+        echo "Invalid village"
+        ;;
+esac
