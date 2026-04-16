f097a00594904a569d8565c943cb75aa39a90ff5
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-16 17:41:22 +0530

    hell

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
+SG_ID="sg-02677d7f13c97966d" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z05013202FKF0ZL12WAOP"
 DOMAIN_NAME="daws88s.online"
