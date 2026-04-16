da51017b952bc7690150a54d7a3af4317e8a100b
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-04-16 06:37:56 +0530

    hello

--- practice.sh
+++ practice.sh
@@ -4,7 +4,7 @@ echo "village name "
 read reply
 echo "street name"
 read street
-if [ village_name -eq 0]; then
+if [ $village_name -eq 0]; then
     echo "correct village"
 else
    echo "wrong village"
